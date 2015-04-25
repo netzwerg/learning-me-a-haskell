7f0a34527451fa630ca2beedb630319f71aab3f3
Rahel Lüthy <rahel.luethy@gmail.com>
2015-04-25 20:22:45 +0200

    More parsing funtionality

--- parsing/parsing.hs
+++ parsing/parsing.hs
@@ -28,3 +28,20 @@ boolFalse = (string "false") *> (pure False)
 -- parse bool "test" "lemon" -> Left "test" expecting "true" or "false"
 bool :: Parser Bool
 bool = boolTrue <|> boolFalse
+
+-- parse stringLiteral "test" "\"hello world\"" -> Right "hello world"
+stringLiteral :: Parser String
+stringLiteral = char '"' *> (many (noneOf ['"'])) <* char '"'
+
+data JSONValue = B Bool | S String deriving (Show)
+
+-- parse jsonBool "test" "true" -> Right (B True)
+jsonBool :: Parser JSONValue
+jsonBool = fmap B bool
+
+-- parse jsonStringLiteral "test" "\"test\"" -> Right (S "test")
+jsonStringLiteral :: Parser JSONValue
+jsonStringLiteral = fmap S stringLiteral
+
+jsonValue :: Parser JSONValue
+jsonValue = jsonBool <|> jsonStringLiteral
