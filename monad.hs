aa6a4d5335bf7fee533bff92a30f786746c2dbdc
Rahel Lüthy <rahel.luethy@gmail.com>
2015-05-07 20:50:28 +0200

    Grokking monads

--- monad.hs
+++ monad.hs
@@ -1,4 +1,9 @@
 -- http://blog.sigfpe.com/2006/08/you-could-have-invented-monads-and.html
+
+import Control.Monad.Writer
+
+--------------------------------------------------------------------------------
+
 f, g :: Float -> Float
 f x = x + 1
 g x = x * 10
@@ -10,5 +15,33 @@ g' x = ((g x), "g")
 bind :: (Float -> (Float, String)) -> ((Float, String) -> (Float, String))
 bind f' (gx, gs) = let (fx, fs) = f' gx in (fx, gs ++ fs)  
 
-composition = bind f' . g' -- composition 4 --> (41,"gf")
+composed = bind f' . g' -- composed 4 --> (41,"gf")
 test = bind (\x -> (x, "st")) . (\x -> (x, "te")) -- test 2 -> (2.0,"test")
+
+--------------------------------------------------------------------------------
+
+three, five :: Int -> [Int]
+three x = take 3 (repeat x)
+five x = take 5 (repeat x)
+
+flatMap :: (Int -> [Int]) -> [Int] -> [Int]
+flatMap f x = concat (map f x) 
+fifteen = flatMap three . five
+
+fifteen' x = do 
+                y <- five x
+                z <- three y
+                return z
+
+--------------------------------------------------------------------------------
+
+f'', g'' :: Float -> Writer String Float  
+f'' x = writer (x + 1,  "f")
+g'' x = writer (x * 10, "g")
+
+-- runWriter $ composed' 5 --> (41.0,"gf")
+composed' x = do
+                y <- g'' x
+                z <- f'' y
+                return z
+        
