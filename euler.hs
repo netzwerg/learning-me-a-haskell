ce4fba8be4aa9913a0a8aa95bcbc6bf7b4e63927
Rahel Lüthy <rahel.luethy@gmail.com>
2015-05-30 17:07:58 +0200

    Solve project euler problem #2

--- euler.hs
+++ euler.hs
@@ -1,1 +1,6 @@
 problem_001 = sum [ x | x <- [0..999], x `mod` 5 == 0 || x `mod` 3 == 0]  
+
+--------------------------------------------------------------------------------
+
+fibs = 1 : 1 : zipWith (+) fibs (tail fibs)
+problem_002 = sum $ filter even $ takeWhile (< 4000000) fibs 
